fadf3ee6f9ff1668c8aa9fbf0d5e58a7dc46a71c
maddi <maddie217@hotmail.com>
2019-04-28 15:25:15 -0600

    SOC-6 create default layout

--- src/main/java/com/maddisportfolio/MvcConfig.java
+++ src/main/java/com/maddisportfolio/MvcConfig.java
@@ -1,9 +1,15 @@
 package com.maddisportfolio;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -11,6 +17,40 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/login").setViewName("login");
     }
+
+    @Bean
+    public ClassLoaderTemplateResolver templateResolver(){
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(){
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new LayoutDialect());
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+
+    @Bean
+    public ViewResolver viewResolver(){
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(springTemplateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+
 
 
 
