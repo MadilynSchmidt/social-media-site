dee07e03e5b11d3025436231a19a8bf51c5204b8
maddi <maddie217@hotmail.com>
2019-06-16 16:23:37 -0600

    SMS-16 Fixes post to posts and changes label to a p.

--- src/main/java/com/maddisportfolio/post/PostController.java
+++ src/main/java/com/maddisportfolio/post/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value = "/post")
+@RequestMapping(value = "/posts")
 public class PostController {
 
 
