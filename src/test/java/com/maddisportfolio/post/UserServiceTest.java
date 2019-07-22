be332d0e07afd15cf9c40f6d1dc7fdecf1f95431
maddi <maddie217@hotmail.com>
2019-07-22 14:38:03 -0600

    SMS-20-2 runs test on UserService and FriendRequestService

---
+++ src/test/java/com/maddisportfolio/post/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.maddisportfolio.post;
+import com.maddisportfolio.friendrequest.FriendRequestDao;
+import com.maddisportfolio.friendrequest.FriendRequestSerivce;
+import com.maddisportfolio.user.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.*;
+
+public class UserServiceTest {
+
+    @InjectMocks
+    UserService serviceUnderTest;
+
+    @Mock
+    UserDao userDao;
+
+    @Mock
+    FriendRequestDao friendRequestDao;
+
+    @Mock
+    FriendRequestSerivce friendRequestSerivce;
+
+    @Mock
+    UserService userService;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        serviceUnderTest = new UserService();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetUser() {
+        //Arrange
+        String loggedInUserEmailAddress = "navi@cat.com";
+        User navi = new User();
+        Mockito.when(userDao.findOneByEmailAddressIgnoreCase(loggedInUserEmailAddress)).thenReturn(navi);
+
+        //Act
+        User getUser = serviceUnderTest.getUser(loggedInUserEmailAddress);
+
+        //Assert
+        Assert.assertEquals(navi, getUser);
+
+    }
+
+}
+
